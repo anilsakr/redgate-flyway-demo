4ac031e4a29ea9fb4158ac9db39f42c6c88bd836
Chris Hawkins <Chris.Hawkins@red-gate.com>
2022-03-04 14:00:03 +0000

    Created Stored Procedure for getting info from Person.Person table

---
+++ Test/schema-model/Stored Procedures/dbo.PersonSPROC.sql
@@ -0,0 +1,25 @@
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+--SET QUOTED_IDENTIFIER ON|OFF
+--SET ANSI_NULLS ON|OFF
+--GO
+CREATE PROCEDURE [dbo].[PersonSPROC]
+
+-- WITH ENCRYPTION, RECOMPILE, EXECUTE AS CALLER|SELF|OWNER| 'user_name'
+AS
+BEGIN
+    SELECT BusinessEntityID,
+           PersonType,
+           NameStyle,
+           Title,
+           FirstName,
+           MiddleName,
+           LastName,
+           Suffix,
+           Demographics,
+           rowguid,
+           ModifiedDate FROM Person.Person
+END
+GO
