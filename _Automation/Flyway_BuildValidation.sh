a69efb0b9f84dce42b841011672874d42c2f1e04
Chris Hawkins <Chris.Hawkins@red-gate.com>
2022-03-07 13:08:06 +0000

    Removed Automation Folder

--- _Automation/Flyway_BuildValidation.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Carrying out Flyway Build Tasks"
-echo "The migrations directory is ${env.flywayLocations}"
-flyway clean migrate info "-url=jdbc:sqlserver://database-1.cjwkywqzgyd0.ap-south-1.rds.amazonaws.com:1433;databaseName=AdventureWorks_Build" '-locations=/var/lib/jenkins/workspace/AdventureWorks_Flyway_main/Test/migrations' "-user=admin" "-password=admin1234"
