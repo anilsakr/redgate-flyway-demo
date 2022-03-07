0b105b139965029c38598e3a6ef990e2431aa3eb
Chris Hawkins <94678272+csnhawkins@users.noreply.github.com>
2022-03-07 14:10:57 +0000

    Chris Migration Script

---
+++ Test/migrations/V002_20220304135913__Chris_Hawkins.sql
@@ -0,0 +1,27 @@
+﻿SET NUMERIC_ROUNDABORT OFF
+GO
+SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+PRINT N'Creating [dbo].[PersonSPROC]'
+GO
+--SET QUOTED_IDENTIFIER ON|OFF
+--SET ANSI_NULLS ON|OFF
+--GO
+CREATE PROCEDURE [dbo].[PersonSPROC]
+
+-- WITH ENCRYPTION, RECOMPILE, EXECUTE AS CALLER|SELF|OWNER| 'user_name'
+AS
+BEGIN
+    SELECT BusinessEntityID,
+           PersonType,
+           NameStyle,
+           Title,
+           FirstName,
+           MiddleName,
+           LastName,
+           Suffix,
+           Demographics,
+           rowguid,
+           ModifiedDate FROM Person.Person
+END
+GO
