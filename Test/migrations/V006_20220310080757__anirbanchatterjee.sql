d3e2dde097c60312784311b893a0f7465347d325
Chatterjee <anirbanchatterjee@deloitte.com>
2022-03-11 13:59:34 +0530

    Added insert statement

---
+++ Test/migrations/V006_20220310080757__anirbanchatterjee.sql
@@ -0,0 +1,14 @@
+﻿INSERT INTO [HumanResources].[Department]
+           ([Name]
+           ,[GroupName]
+           ,[ModifiedDate])
+     VALUES
+           ('Logistic', 'Supply Chain Management', SYSDATETIME()),
+		   ('SKU', 'Inventory Management', SYSDATETIME())
+GO
+
+
+
+UPDATE [HumanResources].[Department]
+SET [Name] = 'Director', [GroupName] = 'Research and Development' WHERE [DepartmentID] = 22 
+GO
